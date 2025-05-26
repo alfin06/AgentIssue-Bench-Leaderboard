3c7b38630d08b8d0c703de25a10dcb0d84aedf10
Alfin Rahardja <alfin06@users.noreply.github.com>
2025-05-26 21:21:02 +0800

    Update router

--- src/router.js
+++ src/router.js
@@ -1,21 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' 
 import Leaderboard from './components/Leaderboard.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/leaderboard',
-  },
-  {
-    path: '/leaderboard',
     name: 'Leaderboard',
     component: Leaderboard,
   },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
-
 export default router
